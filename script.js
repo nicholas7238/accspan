2b51f82f2fb98fe111718365e62d835d9c9266d5
nicholct <67173781+nicholct@users.noreply.github.com>
2021-08-06 14:29:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ loadSong(songs[songIndex]);
 // Update song details
 function loadSong(song) {
   title.innerText = song;
-  audio.src = `music/${song}.mp3`;
-  cover.src = `images/${song}.jpg`;
+  audio.src = `audio/${song}.mp3`;
+  cover.src = `images/${song}.png`;
 }
 
 // Play song
